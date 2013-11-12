63808d1c45075a6742b9c0be71abf173e1c29ffd
mgechev <mgechev@gmail.com>
2013-11-12 14:15:32 +0200

    Basic midway test included

---
+++ demo/test/midway/modules.spec.js
@@ -0,0 +1,6 @@
+describe('Modules', function () {
+  'use strict';
+  it('should has angular-transitions defined', function () {
+    expect(angular.module('angular-transitions')).not.toBeFalsy();
+  });
+});
